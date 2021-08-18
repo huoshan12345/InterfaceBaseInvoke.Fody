0cedd2600a796b9e89f2d003d30e3ad8ce05eaf4
huoshan12345 <huoshan12345@hotmail.com>
2021-08-18 10:41:12 +0800

    fix

--- src/InterfaceBaseInvoke.Tests/ClassTestsBase.cs
+++ src/InterfaceBaseInvoke.Tests/ClassTestsBase.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 using Fody;
 using InterfaceBaseInvoke.Fody;
 using InterfaceBaseInvoke.Tests.AssemblyToProcess;
