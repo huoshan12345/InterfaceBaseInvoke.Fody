0cb0a481b65345ca7e441bcf981b987aea3a9ff7
huoshan12345 <huoshan12345@hotmail.com>
2024-11-06 10:20:51 +0800

    add x86 tests

--- src/InterfaceBaseInvoke.Fody/Processing/References.cs
+++ src/InterfaceBaseInvoke.Fody/Processing/References.cs
@@ -12,36 +12,13 @@ public sealed class References
     }
 }
 
-public sealed class TypeReferences
+public sealed class TypeReferences(ModuleWeavingContext Context)
 {
-    public TypeReference RuntimeMethodHandle { get; }
-    public TypeReference IntPtr { get; }
-    public TypeReference Int32 { get; }
-    public TypeReference Environment { get; }
-    public TypeReference Boolean { get; }
-
-    public TypeReferences(ModuleWeavingContext context)
-    {
-        Environment = context.ImportReference(typeof(Environment));
-        IntPtr = context.ImportReference(typeof(IntPtr));
-        Int32 = context.ImportReference(typeof(int));
-        RuntimeMethodHandle = context.ImportReference(typeof(RuntimeMethodHandle));
-        Boolean = context.ImportReference(typeof(bool));
-    }
+    public TypeReference RuntimeMethodHandle { get; } = Context.ImportReference(typeof(RuntimeMethodHandle));
+    public TypeReference IntPtr { get; } = Context.ImportReference(typeof(IntPtr));
 }
 
-public sealed class MethodReferences
+public sealed class MethodReferences(ModuleWeavingContext Context, TypeReferences Types)
 {
-    public MethodReference FunctionPointer { get; }
-    public MethodReference ToInt32 { get; }
-    public MethodReference ToInt64 { get; }
-    public MethodReference Is64BitProcess { get; }
-
-    public MethodReferences(ModuleWeavingContext context, TypeReferences types)
-    {
-        FunctionPointer = MethodRefBuilder.MethodByNameAndSignature(context, types.RuntimeMethodHandle, nameof(RuntimeMethodHandle.GetFunctionPointer), 0, types.IntPtr.ToTypeRefBuilder(context), []).Build();
-        ToInt32 = MethodRefBuilder.MethodByName(context, types.IntPtr, nameof(IntPtr.ToInt32)).Build();
-        ToInt64 = MethodRefBuilder.MethodByName(context, types.IntPtr, nameof(IntPtr.ToInt64)).Build();
-        Is64BitProcess = MethodRefBuilder.PropertyGet(context, types.Environment, nameof(Environment.Is64BitProcess)).Build();
-    }
+    public MethodReference FunctionPointer { get; } = MethodRefBuilder.MethodByNameAndSignature(Context, Types.RuntimeMethodHandle, nameof(RuntimeMethodHandle.GetFunctionPointer), 0, Types.IntPtr.ToTypeRefBuilder(Context), []).Build();
 }
