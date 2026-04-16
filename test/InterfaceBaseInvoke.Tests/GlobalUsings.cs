9f61c20841fd219a3304181d1bd8e36abf6e7cc2
huoshan12345 <huoshan12345@hotmail.com>
2026-04-16 18:42:06 -0230

    use cpm

--- test/InterfaceBaseInvoke.Tests/GlobalUsings.cs
+++ test/InterfaceBaseInvoke.Tests/GlobalUsings.cs
@@ -2,12 +2,10 @@
 
 global using System;
 global using System.Collections.Generic;
-global using System.Text;
 global using Fody;
-global using InterfaceBaseInvoke.Fody;
 global using InterfaceBaseInvoke.Tests.Support;
 global using Mono.Cecil;
 global using Mono.Cecil.Cil;
 global using MoreFodyHelpers.Extensions;
 global using Xunit;
-global using Xunit.Abstractions;
+global using TestResult = Fody.TestResult;
--- test/InterfaceBaseInvoke.Tests/Support/InvalidAssemblyOnlyDebugFactAttribute.cs
+++ test/InterfaceBaseInvoke.Tests/Support/InvalidAssemblyOnlyDebugFactAttribute.cs
@@ -1,14 +1,14 @@
-using System.Diagnostics;
-using System.Reflection;
+using System.Runtime.CompilerServices;
 
-namespace InterfaceBaseInvoke.Tests.Support
+namespace InterfaceBaseInvoke.Tests.Support;
+
+public class InvalidAssemblyOnlyDebugFactAttribute : FactAttribute
 {
-    public class InvalidAssemblyOnlyDebugFactAttribute : FactAttribute
+    public InvalidAssemblyOnlyDebugFactAttribute(
+        [CallerFilePath] string? sourceFilePath = null,
+        [CallerLineNumber] int sourceLineNumber = -1)
+        : base(sourceFilePath, sourceLineNumber)
     {
-        public override string? Skip
-        {
-            get => base.Skip ?? (InvalidAssemblyToProcessFixture.IsDebug ? null : "Inconclusive in release builds");
-            set => base.Skip = value;
-        }
+        Skip = InvalidAssemblyToProcessFixture.IsDebug ? null : "Inconclusive in release builds";
     }
 }
